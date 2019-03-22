4d8fe810d84d06d519698b303f1851f1781073dc
Edward J <edjeong99@gmail.com>
2019-03-22 10:23:58 -0400

    assign appropriate path

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,9 +6,18 @@
  */
 int main(int argc, char **argv)
 {
+  char *path;
+  DIR *cur_dir;
   // Parse command line
+  
+  if(argv[1]) path = argv[1] ;
+  else path = argv[0];
+
+  printf("Path = %s\n", path);
 
   // Open directory
+  cur_dir = opendir(path);
+
 
   // Repeatly read and print entries
 

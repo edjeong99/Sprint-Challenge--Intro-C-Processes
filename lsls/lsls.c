00cc34260ec7cfd32db3ff3f62ba6a14793c76ed
Edward J <edjeong99@gmail.com>
2019-03-22 10:37:08 -0400

    print file names

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -6,22 +6,37 @@
  */
 int main(int argc, char **argv)
 {
+  printf("argv[0] = %s\n", argv[0]);
   char *path;
   DIR *cur_dir;
+  struct dirent * open_dir_point;
+
   // Parse command line
   
   if(argv[1]) path = argv[1] ;
-  else path = argv[0];
+  else path = ".";
 
   printf("Path = %s\n", path);
 
   // Open directory
   cur_dir = opendir(path);
+  // if opendir failed, exit
+  if(cur_dir == NULL){
+    printf("Open returned NULL");
+    exit(0);
+  }
 
 
   // Repeatly read and print entries
+  while(1){
+
+    open_dir_point = readdir(cur_dir);
+    if(open_dir_point == NULL) break;
+    printf("%s\n",open_dir_point->d_name);
+
+  }
 
   // Close directory
-
+  closedir(cur_dir);
   return 0;
 }

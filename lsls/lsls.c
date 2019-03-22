e8a5adb1b660042c7d400993206b8cd78833b09e
Edward J <edjeong99@gmail.com>
2019-03-22 11:15:48 -0400

    correctly print file size and name

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
-  printf("argv[0] = %s\n", argv[0]);
+  // printf("argv[0] = %s\n", argv[0]);
   char *path;
+  char file_path[100] = "";
   DIR *cur_dir;
   struct dirent * open_dir_point;
+  struct stat buf;
 
   // Parse command line
   
   if(argv[1]) path = argv[1] ;
   else path = ".";
 
-  printf("Path = %s\n", path);
+  // printf("Path = %s\n", path);
 
   // Open directory
   cur_dir = opendir(path);
@@ -29,10 +33,18 @@ int main(int argc, char **argv)
 
   // Repeatly read and print entries
   while(1){
-
+    
     open_dir_point = readdir(cur_dir);
     if(open_dir_point == NULL) break;
-    printf("%s\n",open_dir_point->d_name);
+
+    
+     strcpy(file_path, path);
+    strcat(file_path, "/");
+    strcat(file_path, open_dir_point->d_name);
+    // printf("file_path = %s\n", file_path);
+     stat(file_path, &buf);
+
+     printf("  %lld  %s\n", buf.st_size, open_dir_point->d_name);
 
   }
 
